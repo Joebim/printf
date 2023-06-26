19b9851ddac8438ab293a11a402b9f64eed11d91
Joebim <josey359@gmail.com>
2023-06-26 11:24:21 -0700

    [FIX] error in file _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
 
+/**
+ * _putchar - function to print character to standard output
+ * @c: character value
+ * Return: write 1, character, 1 to standard output
+ */
+
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
